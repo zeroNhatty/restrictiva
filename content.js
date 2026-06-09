3a0559ca1d4ed68876e4637d64285a8c3fc2f2fd
guywithzeromotive <ntnesi01@gmail.com>
2026-06-09 19:06:21 +0300

    feat: disable on shorts row

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-alert("hello from restrictiva");
+console.log("restrictiva has started!");
+
+setTimeout(() => {
+
+    const shortsSection = document.querySelector('#dismissible.style-scope.ytd-rich-shelf-renderer');
+    shortsSection.style.display = 'none';
+    shortsSection.hidden = true;
+    console.log("Shorts section hidden successfully!");
+}, 6000);
